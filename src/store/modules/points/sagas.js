473dc2ee4d6b3380e33c102023209626cbc43e86
Argério Queiroz da Silva Filho <argerioaf@gmail.com>
2021-06-15 21:51:32 -0300

    Componentes para vincular e fazer restore do stream elements finalizado

--- src/store/modules/points/sagas.js
+++ src/store/modules/points/sagas.js
@@ -3,6 +3,7 @@ import * as Points from '../../../services/Points';
 import  * as actionType from './actionTypes';
 import * as actions from './actions';
 import { setErrorGeneral } from '../error/actions';
+import * as actionsUser from './../user/actions';
 
 function* loadRedeemPointsWorker({params}) {
   yield put(actions.setLoadingRedeemPoints(true));
@@ -34,8 +35,40 @@ function* loadRedeemPointsWorker({params}) {
   }
 }
 
+function* restorePointsStreamElementsWorker() {
+  yield put(actions.setLoadingRedeemPoints(true));
+  yield put(actions.setStatusRedeemPoints(0));
+  yield put(actions.setResponseRedeemPoints({}));
+  yield put(actions.setErrorRedeemPoints(''));
+  try {
+    const response = yield call(Points.restorePointsStreamElementsService,{});
+    yield put(actions.setRedeemPoints(response.data));
+    yield put(actions.setLoadingRedeemPoints(false));
+    yield put(actions.setErrorRedeemPoints(''));
+    yield put(setErrorGeneral('',false,0));
+    yield put(actions.setResponseRedeemPoints(response));
+    yield put(actions.setStatusRedeemPoints(response.status));
+    yield put(actionsUser.loadInfoUser());
+  } catch (error) {
+    yield put(actions.setLoadingRedeemPoints(false));
+    yield put(actions.setStatusRedeemPoints(error.status));
+    if (error.response) {
+      yield put(setErrorGeneral(error.response.data.message,true,error.response.status));
+      yield put(actions.setErrorRedeemPoints(error.response.data.message));
+      yield put(actions.setStatusRedeemPoints(error.response.status));
+    } else if (error.request) {
+      yield put(setErrorGeneral(error.message,true,error.request.status));
+      yield put(actions.setErrorRedeemPoints({ data: error.message }));
+      yield put(actions.setStatusRedeemPoints(error.request.status));
+    } else {
+      yield put(actions.setErrorRedeemPoints({ data: error.message }));
+    }
+  }
+}
+
 function* watcherAnalise() {
   yield takeLatest(actionType.LOAD_REDEEM_POINTS, loadRedeemPointsWorker);
+  yield takeLatest(actionType.RESTORE_POINTS_STREAM_ELEMENTS, restorePointsStreamElementsWorker);
 }
 
 export default function* saga() {
